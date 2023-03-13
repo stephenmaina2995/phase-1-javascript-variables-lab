2fbf8551884052a3be4b5a9b78d1e47e63136dbc
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-03-13 11:27:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+const companyName='Scuber';
+// const companyName
+// var mostProfitableNeigbourhood="Chelsea";
+let mostProfitableNeighborhood='Chelsea';
+
+let companyCeo="Susan Smith";
